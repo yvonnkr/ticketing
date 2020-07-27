e7e38627d11a9ffdc516b03ff05fbf0bf816d88a
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-07-27 20:43:32 +0100

    change cookie to use http and change base-url client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,9 @@ export const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      // baseURL:
+      //   "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local", //for dev
+      baseURL: "http://www.yvonnkr-ticketing-prod.xyz",
       headers: req.headers,
     });
   } else {
